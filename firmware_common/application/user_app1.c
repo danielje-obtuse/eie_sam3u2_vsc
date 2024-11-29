38820e220ad3a1026523e1f56cf8c3ddcb27b32c
danielje <150391560+danielje-obtuse@users.noreply.github.com>
2024-11-28 19:03:06 -0700

    Update user_app1.c

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -96,7 +96,7 @@ Promises:
 void UserApp1Initialize(void)
 {
   u8 *u8Message = "Hello World";
-  LcdClearChars(LINE1_START_ADDR + 13, 3);
+  LcdClearChars(LCD_CLEAR_CMD);
   LcdMessage(LINE1_START_ADDR, u8Message);
   /* If good initialization, set state to Idle */
   if( 1 )
